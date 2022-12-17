a8556917f5ba79a1e1d68890bbe4c55f95df2a68
jeremias <jeremiasprogrammer@gmail.com>
2022-12-16 23:36:15 -0300

    finish V1 project

--- src/main.py
+++ src/main.py
@@ -13,14 +13,64 @@ except:
 
 from net_observer import NetObserver
 import time
+import signal
+import sys
+from datetime import datetime
 
 def main():
+    count_tests = 0
+    most_download_rate = 0
+    most_upload_rate = 0
+    less_ms_rate = 1
+    date_without_connection_start = None
+    time_without_connection = 0 
+    count_lost_connections = 0 #Utilizar esta variável para contar quantas vezes a net caiu 
+    lost_connection = False
+    
     net_observer = NetObserver()
-    while True:
-        msg = 'connected' if net_observer.connect() else 'no internet!'
-        net_observer.write_log(msg)
-        time.sleep(5)
+    def before_exit(signum, frame):
+        msg = "Quantidade de testes de conexão: "+ str(count_tests)
+        net_observer.write_log(msg, 1)
+        net_observer.write_long_log(msg, 1)
+        
+        net_observer.write_log("Finalizado")
+        net_observer.write_long_log("Finalizado")
+        sys.exit(1)
 
+    signal.signal(signal.SIGINT, before_exit)
+    signal.signal(signal.SIGTERM, before_exit)
+    signal.signal(signal.SIGHUP, before_exit)
+
+    while True:
+        time_start = time.time()
+        has_internet = net_observer.connect()
+        time_finish = time.time() - time_start
+        
+        if has_internet:
+            if lost_connection == True:
+                lost_connection = False
+                net_observer.write_log("Conexão restabelecida")
+                net_observer.write_long_log("Conexão restabelecida")
+                time_without_connection = net_observer.time_without_connection(date_without_connection_start, datetime.now())
+                net_observer.write_log(time_without_connection, 2)
+                net_observer.write_long_log(time_without_connection, 2)
+            msg = 'Conectado'
+            msg += ' - Download: '+str(net_observer.download_test())+' MB'
+            msg += ' - Upload: '+str(net_observer.upload_test())+' MB'
+            msg += ' - '+str(round(time_finish, 2))+' ms'
+        else:
+            if lost_connection == False:
+                lost_connection = True
+                date_without_connection_start = datetime.now()
+                net_observer.write_log("Conexão perdida")
+                net_observer.write_long_log("Conexão perdida")
+            msg = 'Sem conexão'
+        
+        count_tests += 1
+        
+        net_observer.write_long_log(msg)
+        time.sleep(2)
+    
 
 if __name__ == "__main__":
     main()
--- src/net_observer.py
+++ src/net_observer.py
@@ -3,31 +3,53 @@ import urllib.request
 import time
 import os
 from datetime import datetime
+import speedtest
+from datetime import datetime
 
 class NetObserver:
     def __init__(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        path_log = os.path.join(dir_path, '..', 'report.log')
-        logging.basicConfig(filename=path_log, encoding='utf-8', level=logging.DEBUG)
-        self.logger = logging.getLogger('NetObserver')
-        self.logger.info('Initialized')
+        self.logger = None
+        self.__initilized_info()
+        self.speed_test = speedtest.Speedtest()
 
     def connect(self):
         try:
-            urllib.request.urlopen('http://google.com') #Python 3.x
+            urllib.request.urlopen('https://google.com') #Python 3.x
             return True
         except:
             return False
     
+    def upload_test(self):
+        return self.__bytes_to_mb(self.speed_test.upload())
+    
+    def download_test(self):
+        return self.__bytes_to_mb(self.speed_test.download())
+
+    def time_without_connection(self, date_start, date_end):
+        sub_date = date_end - date_start
+        str_result = "Conexão perdida por: "
+        if sub_date.total_seconds() < 59:
+            str_result += str(round(sub_date.total_seconds(), 2)) + "s"
+            return str_result
+        str_result += str(round((sub_date.total_seconds()/60), 2)) + "min"
+        return str_result
+
     def write_log(self, msg, type=1):
         """
             Type 1 = Info
             Type 2 = Warning
             Type 3 = Error
         """
+        
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        
+        #Initializing o log long
+        path_log = os.path.join(dir_path, '..', 'report.log')
+        self.logger = self.__config_log(path_log, "NET Observer System")
+
         act_datetime = datetime.now()
         act_datetime = act_datetime.strftime('%d/%m/%Y %H:%M')
-        msg = "[" + act_datetime + "] " + msg
+        msg = "[" + act_datetime + "] " + str(msg)
         
         
         if type == 1:
@@ -39,3 +61,65 @@ class NetObserver:
         else:
             self.logger.error('Invalid type to write log')
     
+    def write_long_log(self, msg, type=1):
+        """
+            Type 1 = Info
+            Type 2 = Warning
+            Type 3 = Error
+        """
+                
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        
+        #Initializing o log long
+        path_log = os.path.join(dir_path, '..', 'report_long.log')
+        self.logger = self.__config_log(path_log, "NET Observer System")
+
+        act_datetime = datetime.now()
+        act_datetime = act_datetime.strftime('%d/%m/%Y %H:%M')
+        msg = "[" + act_datetime + "] " + msg        
+        
+        if type == 1:
+            self.logger.info(msg)
+        elif type == 2:
+            self.logger.warning(msg)
+        elif type == 3:
+            self.logger.error(msg)
+        else:
+            self.logger.error('Invalid type to write log')
+    
+    def __config_log(self, path, name="NET Observer"):
+        fileh = logging.FileHandler(path, 'a')
+        formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+        fileh.setFormatter(formatter)
+
+        log = logging.getLogger(name)  # root logger
+        for hdlr in log.handlers[:]:  # remove all old handlers
+            log.removeHandler(hdlr)
+        log.addHandler(fileh)      # set the new handler
+        log.setLevel(logging.DEBUG)
+        return log
+
+    def __initilized_info(self):
+        msg = "Inicializado"
+        
+        act_datetime = datetime.now()
+        act_datetime = act_datetime.strftime('%d/%m/%Y %H:%M')
+        msg = "[" + act_datetime + "] " + msg
+        
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        
+        #Initializing o log long
+        path_log = os.path.join(dir_path, '..', 'report_long.log')
+        self.logger = self.__config_log(path_log, "NET Observer System")
+        self.logger.info(msg)
+        
+        #Initializing short log
+        path_log = os.path.join(dir_path, '..', 'report.log')
+        self.logger = self.__config_log(path_log, "NET Observer System")
+        self.logger.info(msg)
+
+
+    def __bytes_to_mb(self, bytes):
+        KB = 1024 # One Kilobyte is 1024 bytes
+        MB = KB * 1024 # One MB is 1024 KB
+        return int(bytes/MB)
